0db9b7badc3db57f3ec89499179ff1db117aa38b
smileham <steven.mileham@gmail.com>
2019-11-18 12:55:39 +0000

    Update code.js
    Change to Person API

--- code.js
+++ code.js
@@ -1,16 +1,24 @@
 function doGet(e) {
-  var theUser = DriveApp.getRootFolder().getOwner();
-  var thePhotoUrl = theUser.getPhotoUrl().replace("=s64", "=s128");
-
-  var theHTML = HtmlService.createTemplateFromFile("Form.html");
-  theHTML.photoUrl = thePhotoUrl;
-  theHTML.userName = theUser.getName();
-
-  return theHTML
-    .evaluate()
-    .addMetaTag("viewport", "width=device-width, initial-scale=1")
-    .setTitle("Gmail Signature")
-    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+    var theHTML = HtmlService.createTemplateFromFile('Form.html')
+    theHTML.photoUrl="";
+    theHTML.userName ="";
+    theHTML.jobTitle = "{Job Title}";
+    theHTML.dept = "{Department}";
+    theHTML.location = "{Location}";
+    theHTML.address = "{Address}";
+    theHTML.phoneNumber = "{Phone Number}";
+    try {
+      var person = People.People.get("people/me", {personFields: 'names,photos'});
+      theHTML.photoUrl = person.photos[0].url.replace("=s100","=s128");
+      theHTML.userName = person.names[0].displayName;
+    }
+    catch (e) {
+      Logger.log(e);
+    }
+  
+    return theHTML.evaluate().addMetaTag("viewport", "width=device-width, initial-scale=1")
+        .setTitle("Gmail Signature")
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME);
 }
 
 function include(filename) {
